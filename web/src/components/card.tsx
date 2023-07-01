7c9201655617d96e7397f17a2cfbd07d0a6dbbed
Nagodo <magnusschoendorff@gmail.com>
2023-07-01 16:13:07 +0200

    Added card interface and attack-damage

--- web/src/components/card.tsx
+++ web/src/components/card.tsx
@@ -4,7 +4,18 @@ import img from '../imgs/Frames/normal.png'
 import img1 from '../imgs/Elements/FireElement.png'
 import img2 from '../imgs/Cards/FirstEdition Collection/CardPictures/g_holo_john_olsen_01.jpg'
 
-export function Card(props: {size: number}) {
+interface CardProps {
+    name: string;
+    health: number;
+    info: string;
+    attack: string;
+    damage: number;
+    cardNum: number;
+    size: number;
+}
+
+
+export function Card(props: CardProps) {
     let scale = 0.025 * props.size;
     let width = 1024 * scale
     let height = 1420 * scale
@@ -24,11 +35,17 @@ export function Card(props: {size: number}) {
                 <img src={img2} />
             </div>
 
-            <p className='name' style={{"fontSize": GetNameFontSize(scale)}}>John Olsen</p>
+            <p className='name' style={{"fontSize": GetNameFontSize(scale)}}>{props.name}</p>
 
-            <p className='health' style={{"fontSize": GetHealthFontSize(scale)}}>1000HP</p>
+            <p className='health' style={{"fontSize": GetHealthFontSize(scale)}}>{props.health}HP</p>
 
-            <p className='info' style={{"fontSize": GetInfoFontSize(scale)}}>Fede førtidspensionist. Elsker sove he. Remolade her</p>
+            <p className='info' style={{"fontSize": GetInfoFontSize(scale)}}>{props.info}</p>
+
+            <p className='attack' style = {{"fontSize": GetAttackFontSize(scale)}}>{props.attack}</p>
+
+            <p className='damage' style = {{"fontSize": GetDamageFontSize(scale)}}>{props.damage}</p>
+
+            <p className='num' style = {{"fontSize": GetCardNumFontSize(scale)}}>{props.cardNum} / 143</p>
 
         </div>
     )
@@ -44,4 +61,16 @@ function GetHealthFontSize(scale: number) {
 
 function GetInfoFontSize(scale: number) {
     return 25 * scale + "vh";
+}
+
+function GetAttackFontSize(scale: number) {
+    return 70 * scale + "vh";
+}
+
+function GetDamageFontSize(scale: number) {
+    return 70 * scale + "vh";
+}
+
+function GetCardNumFontSize(scale: number) {
+    return 40 * scale + "vh";
 }

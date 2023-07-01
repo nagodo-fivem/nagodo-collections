b40a09c7d0a1e7718636ce4e29d0e3f749291a51
Nagodo <magnusschoendorff@gmail.com>
2023-07-01 22:12:21 +0200

    More propeerties for the cards

--- web/src/components/management/m-card-editor.tsx
+++ web/src/components/management/m-card-editor.tsx
@@ -8,27 +8,91 @@ interface EditorCardData {
     attack: string;
     damage: number;
     cardNum: number;
+    size?: number;
 }
 
 interface CardTextInputProps {
     title: string;
+    type?: string;
     value: string | number;
     onChange: Function;
 }
 
-function CardTextInput(props: CardTextInputProps) {   
+interface CardScaleData {
+    cardSize: number;
+    sliderSize: number;
+}
 
+
+function CardTextInput(props: CardTextInputProps) {  
+    const [expanded, setExpanded] = useState<boolean>(false);
+
+    
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
         props.onChange(e.target.value);
     }
 
+    function handleColorChange(e: React.ChangeEvent<HTMLInputElement>) {
+
+    }
+    
+    function handleExpand() {
+        setExpanded(!expanded);
+    }
+    
+    let className = "card-text-input";
+    let iconClassName = "fa-solid fa-chevron-down expand";
+    let additionalClassName = "additional";
+    if (expanded) {
+        className += " expanded"; 
+        iconClassName = "fa-solid fa-chevron-up expand";
+        additionalClassName += " expanded";
+    }
+    
+
     return (
-        <div className="card-text-input">
+        <div className = {className}>
             <p className='title'>{props.title}</p>
 
-            <input type="text" value={props.value} onChange={handleChange} />
+            <i className = {iconClassName} onClick={handleExpand}></i>
+
+            <input type = {props.type} value={props.value} onChange={handleChange} autoComplete='none'/>
+
+            <div className = {additionalClassName}>
+                <p className='color'>Color</p>
+                <div className='current-color-preview'></div>
+                <p className='use-default'>Use default collection color</p>
+                <input className='use-default-checkbox' type = "checkbox" />
+            </div>
         </div>
     );
+}
+
+function Preview(props: EditorCardData) {
+    const [size, setSize] = useState<CardScaleData>({cardSize: 1.5, sliderSize: 15});
+
+    function handleSizeChange(e: React.ChangeEvent<HTMLInputElement>) {
+        let sliderSize = parseInt(e.target.value);
+        let cardSize = sliderSize / 10;
+        setSize({cardSize: cardSize, sliderSize: sliderSize});
+    }
+
+    return (
+        <div className='preview-section'>
+
+            <div className="scale">
+                <input type="range" min="1" max="19" value={size.sliderSize} onChange={handleSizeChange} />
+            </div>
+
+            <div className='container'>
+                <div className="preview">
+                    <Card name = {props.name} health = {props.health} info = {props.info} attack = {props.attack} damage = {props.damage} cardNum = {13} size={size.cardSize} />
+                </div>
+            </div>
+            
+        </div>
+        
+    )
 }
 
 export function CardEditor() {
@@ -63,18 +127,14 @@ export function CardEditor() {
         <div className="card-editor">
             <div className="input">
                 <CardTextInput title = 'Name' value = {cardData.name} onChange={handleNameChange} />
-                <CardTextInput title = 'Health' value = {cardData.health} onChange={handleHealthChange} />
+                <CardTextInput title = 'Health' type='number' value = {cardData.health} onChange={handleHealthChange} />
                 <CardTextInput title = 'Info' value = {cardData.info} onChange={handleInfoChange} />
                 <CardTextInput title = 'Attack' value = {cardData.attack} onChange={handleAttackChange} />
                 <CardTextInput title = 'Damage' value = {cardData.damage} onChange={handleDamageChange} />
             </div>
 
-            <div className="preview">
-                <Card name = {cardData.name} health = {cardData.health} info = {cardData.info} attack = {cardData.attack} damage = {cardData.damage} cardNum = {13} size={1.8} />
-            </div>
+            <Preview name = {cardData.name} health = {cardData.health} info = {cardData.info} attack = {cardData.attack} damage = {cardData.damage} cardNum = {13} size = {1.8} />
         </div>
     )
 
-
-    return null;
 }

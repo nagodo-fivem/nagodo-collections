87c7188a879ad97832e794c19ceb8ab5a4783c2b
Nagodo <89977444+Nagodo@users.noreply.github.com>
2023-08-24 20:56:25 +0200

    stuff

--- web/src/components/adminmenu/collections/manage-collection.tsx
+++ web/src/components/adminmenu/collections/manage-collection.tsx
@@ -5,6 +5,17 @@ export function ManageCollection() {
     return (
         <div className='allCards'>
             <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
+            <Card name = "John Olsen" health = {310} info = "Førtidspension. Højde: 150cm. Vægt: 170kg" attack = "Hvad glor du på?" damage = {120} cardNum = {13} size = {1}/>
         </div>
     );
     
--- web/src/components/adminmenu/properties/property-editor.tsx
+++ web/src/components/adminmenu/properties/property-editor.tsx
@@ -9,13 +9,35 @@ interface PropertyEditorProps {
 function PropertySection(props: PropertyEditorProps) {
     const [expanded, setExpanded] = useState<boolean>(false);
 
-    function Header() {
-        
+    function Header(props: {label: string, callback: Function}) {
+
+        function getIconName() {
+            return expanded ? 'fa-solid fa-chevron-up' : 'fa-solid fa-chevron-down';
+        }
+
+        return (
+            <div className='header' onClick={() => {props.callback(!expanded)}}>
+                <p className='title'>{props.label}</p>
+
+                <i className = {getIconName()}></i>
+            </div>
+        );
+    }
+
+    function Content(props: {shouldShow: boolean}) {
+        if (!props.shouldShow) return null;
+
+        return (
+            <div className='section-content'>
+
+            </div>
+        )
     }
 
     return (
         <div className="property-section">
-
+            <Header label = {props.label} callback={setExpanded} />
+            <Content shouldShow = {expanded}/>
         </div>
     );
 }
